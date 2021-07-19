c0a5e944875f7d40ad6d7d86b28a902d757ae56a
taikinakano <kabayaki3323@gmail.com>
2021-07-19 13:40:41 +0000

    カレンダー機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,9 @@ class User < ApplicationRecord
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :followings, through: :relationships, source: :followed
-  has_many :messages,                  dependent: :destroy
-  has_many :entries,                   dependent: :destroy
+  has_many :entries#DM機能始まり
+  has_many :messages
+  has_many :rooms, through: :entries#DM機能終わり
   has_many :favorites,                 dependent: :destroy
   has_many :bookmarks,                 dependent: :destroy
   has_many :shop_comments,             dependent: :destroy
